b79a49d01b46443c588686f730be070e7f6a9914
dtrts <djt210@gmail.com>
2019-07-29 18:11:11 +0100

    Write code for journeys

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,12 +4,13 @@ class Oystercard
   FARE = 2
   ERR_BALANCE_LIMIT = 'Failed to change card as balance would pass the limit of #{MAXIMUM_BALANCE}.'.freeze
   ERR_TOUCH_IN_NO_FUNDS = 'Failed to start journey as there''s insufficient funds (less than #{MINIMUM_BALANCE}) on your card.'.freeze
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journeys
 
   def initialize(balance = 0)
     raise ERR_BALANCE_LIMIT if balance > MAXIMUM_BALANCE
 
     @balance = balance
+    @journeys = []
   end
 
   def top_up(amount)
@@ -28,7 +29,8 @@ class Oystercard
     @entry_station = entry_station
   end
 
-  def touch_out
+  def touch_out(exit_station)
+    @journeys << { entry_station: @entry_station, exit_station: exit_station }
     @entry_station = nil
     deduct(FARE)
   end
